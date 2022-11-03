a9bdb06466455495877cf67a8cebab44d179594a
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-11-03 18:23:17 -0500

    Set details

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PizzasContext } from "../../routes/Router";
 import { getPizzas } from "../../services/getApiInfo";
-import './home.scss'
+import './home.scss';
+import { useNavigate} from "react-router-dom";
+
 
 function Home() {
 
     const { pizzas, setPizzas } = useContext(PizzasContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
         getPizzasInfo()
@@ -19,6 +22,11 @@ function Home() {
         setLoad(true)
     }
 
+    const pizzaDetail = (pizza) => {
+        const pizzaName = pizza.name.toLowerCase();
+        navigate(`/details/${pizzaName}`)
+    }
+
     return (
         <>
             <div className="container">
@@ -28,10 +36,10 @@ function Home() {
                             <ul className="container__card">
                                 {
                                     pizzas.map((pizza, index) =>
-                                        <li key={index} className="card">
+                                        <li key={index} className="card" onClick={()=>{pizzaDetail(pizza)}}>
                                             <img src={pizza.image} alt="Pizza image" className="pizzaCard" />
-                                            <div className="infoProduct">
-                                                <h3>
+                                            <div className="infoProduct" >
+                                                <h3 >
                                                     {pizza.description}
                                                 </h3>
                                                 <button><span>${pizza.price}</span></button>
