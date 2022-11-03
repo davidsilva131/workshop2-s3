805c0da276db7ea1ce3c1e761518f7f1813731aa
davidsilva131 <davidsilvac131@gmail.com>
2022-11-02 20:03:17 -0500

    Adding contexts

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,6 +7,8 @@ import Login from "../components/pages/Login";
 import Register from "../components/pages/Register";
 
 export const UserContext = createContext()
+export const ShopContext = createContext()
+export const PizzasContext = createContext()
 
 const Router = () => {
     const [user, setUser] = useState({
@@ -16,6 +18,9 @@ const Router = () => {
         name: ''
     });
 
+    const [shop, setShop] = useState({})
+    const [pizzas, setPizzas] = useState([])
+
     return (
 
         <BrowserRouter>
@@ -23,15 +28,25 @@ const Router = () => {
                 user,
                 setUser,
             }}>
-                <Routes>
-                    <Route element={<Layout />}>
-                        <Route path="/" element={<Login />} />
-                        <Route path="register" element={<Register />} />
-                    </Route>
-                    <Route element={<LayoutHome />}>
-                        <Route path="home" element={<Home />} />
-                    </Route>
-                </Routes>
+
+                <PizzasContext.Provider value={[]}>
+                    <Routes>
+                        <Route element={<Layout />}>
+                            <Route path="/" element={<Login />} />
+                            <Route path="register" element={<Register />} />
+                        </Route>
+
+
+                        <ShopContext.Provider value={{}}>
+                            <Route element={<LayoutHome />}>
+                                <Route path="home" element={<Home />} />
+                            </Route>
+
+
+                        </ShopContext.Provider>
+
+                    </Routes>
+                </PizzasContext.Provider>
             </UserContext.Provider>
         </BrowserRouter>
 
