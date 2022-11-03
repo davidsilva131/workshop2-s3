b6b00d1d9edc087e4add58632abea9d6120aa6e5
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-11-02 21:45:09 -0500

    Finish signin page design

--- src/components/login/Logo.jsx
+++ src/components/login/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
         <>
             <aside className="login__logo">
                 <img src="https://imgs.search.brave.com/hEGGV0ikghdlBgLt7hxj1Tt9CDIkZDrN07uI7_-sBG8/rs:fit:856:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfNDI2MDAxLnBu/Zw" alt="pizza logo" />
-                <h1>PiSassScript</h1>
+                <h1>PiSassScript</h1>                
             </aside>
         </>
     )
